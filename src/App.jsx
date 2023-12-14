cd8437c32aa422b2297513d756e05f2a2ebe1503
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-12-14 13:05:18 +0530

    api done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+	import.meta.env.VITE_URL,
+	import.meta.env.VITE_API_KEY
+);
 
 function App() {
-	return <>App</>;
+	const [rows, setrows] = useState(null);
+
+	useEffect(() => {
+		getRows();
+	}, []);
+
+	async function getRows() {
+		const { data, error } = await supabase.from("transactions").select();
+		setrows(data);
+		console.log(data);
+		console.log(error);
+	}
+
+	return (
+		<div>{rows === null ? <p>Loading...</p> : <div>DATA FOUND</div>}</div>
+	);
 }
 
 export default App;
